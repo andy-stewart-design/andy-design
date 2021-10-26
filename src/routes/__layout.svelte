5ec40e4910171407625a14f28f932468be3abc9d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2021-10-25 18:18:39 -0700

    Added intro animation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,24 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	// import PgTransPrimary from '$lib/pageTransitions/PgTransPrimary.svelte';
+	import { fade } from 'svelte/transition';
 	import Nav from '$lib/Nav.svelte';
+
+	let isLoaded = true;
+
+	onMount(() => {
+		isLoaded = false;
+	});
 </script>
+
+{#if isLoaded}
+	<div
+		transition:fade={{ duration: 1000, delay: 500 }}
+		class="w-screen h-screen absolute top-0 left-0 bg-white"
+	/>
+{/if}
 
 <Nav activeLink={$page.path} />
 
-<!-- <PgTransPrimary refresh={$page.path}> -->
 <slot />
-<!-- </PgTransPrimary> -->
