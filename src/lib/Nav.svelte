eef2720398dfc870eb32128695cae0a6346848da
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2021-11-09 17:50:56 -0800

    Updated Nav Positioning

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,29 +3,27 @@
 	import AndyMark from '$lib/SVG/AndyMark.svelte';
 </script>
 
-<nav
-	class="flex justify-between fixed top-0 left-0 w-full py-4 px-4 md:px-8 z-30 mix-blend-difference"
->
-	<a href="/">
-		<AndyMark />
-	</a>
+<a class="fixed top-8 left-4 md:left-8 w-10 h-10 z-30 mix-blend-difference" href="/">
+	<AndyMark />
+</a>
 
-	<div>
-		<a
-			sveltekit:prefetch
-			href="/"
-			class:current={activeLink === '/'}
-			class="font-bold text-white opacity-60 hover:opacity-100 transition-opacity duration-500 py-2 mr-4"
-			>Home</a
-		>
-		<a
-			sveltekit:prefetch
-			href="/about"
-			class:current={activeLink === '/about'}
-			class="font-bold text-white opacity-60 hover:opacity-100 transition-opacity duration-500 py-2 mr-4"
-			>About</a
-		>
-	</div>
+<nav
+	class="flex items-center justify-end fixed top-8 right-4 md:right-8 h-10 z-30 mix-blend-difference"
+>
+	<a
+		sveltekit:prefetch
+		href="/"
+		class:current={activeLink === '/'}
+		class="font-bold text-white opacity-60 hover:opacity-100 transition-opacity duration-500 py-2 ml-4"
+		>Home</a
+	>
+	<a
+		sveltekit:prefetch
+		href="/about"
+		class:current={activeLink === '/about'}
+		class="font-bold text-white opacity-60 hover:opacity-100 transition-opacity duration-500 py-2 ml-4"
+		>About</a
+	>
 </nav>
 
 <style>
